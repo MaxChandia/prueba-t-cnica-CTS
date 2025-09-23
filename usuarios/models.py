8c7421e170bd488d685aebc5b379c1dadd480ca7
MaxChandia <maximiliano.chandiaf@gmail.com>
2025-09-23 20:04:10 -0300

    feat/main: Creación de aplicación Django (usuarios) y modelos de usuario

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,8 +1,32 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('El campo de Email debe ser configurado')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('El superusuario debe tener is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('El superusuario debe tener is_superuser=True.')
+        
+        return self.create_user(email, password, **extra_fields)
 
 class User(AbstractUser):
     full_name = models.CharField(max_length=150, blank=True, null=True)
+    username = None
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15, unique=True, blank=True, null=True)
     is_verified = models.BooleanField(default=False)
@@ -10,10 +34,13 @@ class User(AbstractUser):
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+    
+    objects = UserManager()
 
     def __str__(self):
         return self.email
 
 # Creamos el modelo User para los usuarios (Concursantes y Administradores) extendiendo AbstractUser para aprovechar las funcionalidades de autenticación de Django. 
+# Utilizamos un UserManager personalizado para manejar la creación de superusuario sin pedir username.
 # Añadimos los campos adicionales solicitados y configuramos el campo email como el identificador único para el inicio de sesión.
 # full_name y phone_number son opcionales, ya que solo los concursantes los necesitan.
